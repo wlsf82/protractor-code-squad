31844155ce65e4f7a4b68d1fde5d2c4aabe5e3c6
wlsf82 <wlsf82@gmail.com>
2015-07-25 19:19:07 -0300

    Added more tests.

--- spec.js
+++ spec.js
@@ -12,4 +12,20 @@ describe ('Home', function () {
 	it ('title', function () {
         helper.verifyTitle('Demo application');
 	});
+
+    it ('h3\'s', function () {
+        var allH3Elements = element.all(by.css('h3'));
+        expect(allH3Elements.getText()).toEqual(['Pages', 'Panels', 'Navigations', 'Layouts', 'Contexts', 'Rules']);
+    });
+
+    // it ('menu items' , function () {
+    //     var mainMenu = element(by.css('.navbar-nav'));
+    //     var menuItems = mainMenu.all(by.css('.ng-binding'));
+    //     expect(menuItems.getText()).toEqual(['A Page', 'Another Page', 'Blog']);
+    // });
+
+    it ('menu items', function () {
+        var menuItems = element.all(by.css('.navbar-nav .ng-binding'));
+        expect(menuItems.getText()).toEqual(['A Page', 'Another Page', 'Blog']);
+    });
 });
