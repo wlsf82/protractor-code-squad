56f20b349371f005226715a4fe6b1e1a63e8fbfb
wlsf82 <wlsf82@gmail.com>
2015-07-25 22:55:07 -0300

    Added news test on login file.

--- signin.spec.js
+++ signin.spec.js
@@ -12,4 +12,16 @@ describe ('Sign in', function () {
 	it ('title', function () {
 		helper.verifyTitle('Sign in');
 	});
+
+	it ('try to login with a invalid user and password', function () {
+		var usernameField = $('#element-sign-in-username');
+		var passwordField = $('#element-sign-in-password');
+		var signinButton = $('#element-sign-in-submit');
+
+		usernameField.sendKeys('invalid');
+		passwordField.sendKeys('invalid');
+		signinButton.click();
+
+		expect(element(by.repeater('error in errors')).getText()).toEqual('Invalid username or password.');
+	});
 });
