0052dbbcf36ec36b3e0ee0d8b040a5b46bf412ec
wlsf82 <wlsf82@gmail.com>
2015-07-30 00:44:08 -0300

    Updated protractor configuration file with browserstack and saucelabs basic configurations.

--- conf.js
+++ conf.js
@@ -6,10 +6,23 @@ var HtmlReporter = require('protractor-html-screenshot-reporter');
 
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
+	// Saucelabs selenium info:
+	// seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+	// Browserstack selenium info:
+	// seleniumAddress: 'http://hub.browserstack.com/wd/hub',
 	specs: ['spec.js', 'signin.spec.js', 'create.account.spec.js'],
-	// capabilities: {
-	// 	browserName: 'chrome'
-	// },
+	capabilities: {
+		browserName: 'chrome',
+		// Saucelabs credentials:
+		// username: 'your saucelabs username here',
+		// accessKey: 'your saucelabs accesskey here',
+		// Browserstack credentials:
+		// 'browserstack.user': 'your browserstack user here',
+		// 'browserstack.key': 'your browserstack key here',
+		// 'browserstack.debug': true,
+		// Optional:
+		// 'name': 'Code Squad - Protractor'
+	},
 	// multiCapabilities : [
 	// 	{
 	// 		browserName: 'chrome'
