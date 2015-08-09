012ed30a7763fe4fbcbe3dc0b018cc1f5c2bdac5
wlsf82 <wlsf82@gmail.com>
2015-08-09 14:15:22 -0300

    Readjusted blog tests and page objects for their meaning, that is to demonstrate false negative tests, false positives, true negatives and true positives.

--- specs/blog.spec.js
+++ specs/blog.spec.js
@@ -15,28 +15,28 @@ describe ('Blog', function () {
   // False negative test
   // This is a test that needs to be refactored, because it's causing the impression of a bug,
   // but the true is that the bug is in the test.
-  xit ('false negative test', function () {
+  it ('false negative test', function () {
     BlogPage.countPostsOnPanel(6);
   });
 
   // False positive test
   // This is a test that needs to be refactored, bacause it's causing the impression that everything is working fine
   // When the true is that we've a masked bug.
-  xit ('false positive test', function () {
-    // expect(BlogPage.title.getText()).toContain('Blog posts');
+  it ('false positive test', function () {
+    expect(BlogPage.title.getText()).toContain('Blog posts');
     // The below line can be used to make this test a positive negative.
-    expect(BlogPage.title.getText()).toEqual('Blog posts');
+    // expect(BlogPage.title.getText()).toEqual('Blog posts');
   });
 
   // Positive negative test
   // This is a true bug
-  xit ('positive negative test', function () {
+  it ('positive negative test', function () {
     expect(BlogPage.addBlogPostButton.isDisplayed()).toBe(false);
   });
 
   // Positive positive test
   // This is a test that is really testing something is that can be used to provide feedback.
-  xit ('positive positive test', function () {
+  it ('positive positive test', function () {
     expect(BlogPage.listOfPosts.isPresent()).toBe(true);
   });
 });
